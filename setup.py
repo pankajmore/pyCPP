895824a0bed27562ab514a66aadc4022016f71a7
Pankaj More <pankajmore@gmail.com>
2012-02-01 10:45:42 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os, sys
+from distutils.core import setup
+
+
+setup(
+    # metadata
+    name='pyCPP',
+    description='C++ compiler in Python',
+    install_requires=['ply'],
+    license='BSD',
+    version='0.01',
+    authors = ['Pankaj More','Satvik Chahuan', 'Atul Agarwal', 'Himanshu Agarwal'],
+    author_email='pankajmore@gmail.com',
+    url='https://github.com/Course/CS335',
+    package=['pyCPP'],
+)
+
+ 
