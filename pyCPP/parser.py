00ea3dd0f0eb1e31ff5e0dd5fa40241459d6f303
Pankaj More <pankajmore@gmail.com>
2012-02-09 16:48:32 +0530

    basic structure for parser created

---
+++ pyCPP/parser.py
@@ -0,0 +1,20 @@
+import yacc
+
+from lex import tokens
+
+#  ---------------------------------------------------------------
+#  ABSTRACT SYNTAX TREE - NODES
+#  ---------------------------------------------------------------
+
+class Node:
+    """Base class for all the nodes in the AST"""
+
+# abstract out the generic methods in this class
+
+# define new child classes for each token (non-terminal symbol) type and inherit from Node
+
+# is it necessary to define separate nodes for Type System?
+
+
+
+# define functions for each production rule and their attribute grammer/action
