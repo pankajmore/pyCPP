a65e258666b714a0f527bb086d8220849153cc83
Pankaj More <pankajmore@gmail.com>
2012-03-17 22:56:23 +0530

    stripped lexer of its class

--- pyCPP/lexer.py
+++ pyCPP/lexer.py
@@ -3,302 +3,254 @@ import re
 import ply.lex as lex
 from ply.lex import TOKEN
 
-#class CPPLexer(object,error_func,type_lookup_func):
-class CPPLexer(object):
-    """ A lexer for the C++ language. After building it, set the
-        input text with input(), and call token() to get new 
-        tokens.
+##
+## All the tokens recognized by the lexer
+##
 
-        The public attribute filename can be set to an initial
-        filaneme, but the lexer will update it upon #line 
-        directives.
+special_characters=('COMMA',
+        'COLON',
+        'SEMICOLON',
+        'LPAREN',
+        'RPAREN',
+        'LBRACKET',
+        'RBRACKET',
+        'LBRACE',
+        'RBRACE',
+        'QUESTION',
+        'TILDE',
+        'POUND',
+        'DOT',    
+        'SINGLE_QUOTE',
+        'DOUBLE_QUOTE',
+        'BACK_SLASH'
+        )
+
+
+operators=('ASSIGN','ARROW',
+        'GREATER',
+        'LESS',
+        'IS_EQ',
+        'NOT_EQ',
+        'GREATER_EQ',
+        'LESS_EQ',
+        'PLUS_PLUS',
+        'MINUS_MINUS', 
+        'PLUS',    
+        'MINUS',
+        'TIMES',   
+        'DIV', 
+        'MODULO', 'DOUBLE_AMPERSAND',
+        'DOUBLE_PIPE',
+        'EXCLAMATION',  'AMPERSAND',
+        'PIPE',
+        'CARET',
+        'EQ_PLUS',
+        'EQ_MINUS',
+        'EQ_TIMES',
+        'EQ_DIV',
+        'EQ_MODULO',
+        )
+
+complex_tokens=('ID',
+        'ILLEGAL_ID',
+        'DNUMBER',
+        'INUMBER',
+        'ILLEGAL_DNUMBER',
+        'ILLEGAL_INUMBER',
+        'LIT_STR',
+        'LIT_CHAR',
+        'COMMENT')
+
+##
+## Reserved keywords
+##
+
+keywords={'bool': 'BOOL',
+        'break': 'BREAK',
+        'case' : 'CASE', 
+        'char'  : 'CHAR',
+        'class' : 'CLASS',
+        'continue' : 'CONTINUE',
+        'default' : 'DEFAULT',
+        'delete' : 'DELETE',
+        'do' : 'DO',
+        'double' : 'DOUBLE',
+        'else' : 'ELSE',
+        'false' : 'FALSE',
+        'float' : 'FLOAT',
+        'for' : 'FOR',
+        'if' : 'IF',
+        'inline' : 'INLINE',
+        'int' : 'INT',
+        'new' : 'NEW',
+        'operator' : 'OPERATOR',
+        'private' : 'PRIVATE',
+        'public' : 'PUBLIC',
+        'protected' : 'PROTECTED',
+        'return' : 'RETURN',
+        'sizeof' : 'SIZEOF',
+        'struct' : 'STRUCT',
+        'switch' :'SWITCH',     
+        'true' :'TRUE',
+        'typename' : 'TYPENAME',
+        'void' : 'VOID',
+        'while' :'WHILE'
+        }
+
+tokens=special_characters+operators+complex_tokens+tuple(keywords.values())
+
+
+# Each token is specified by writing a regular expression rule. 
+# Each of these rules are are defined by making declarations with a 
+# special prefix t_ to indicate that it defines a token.
+
+t_ARROW = r'->'
+t_ASSIGN = r'='
+t_COMMA = r','
+t_COLON = r':'
+t_SEMICOLON = r';'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
+t_LBRACE = r'{'
+t_RBRACE = r'}'
+t_GREATER = r'>'
+t_LESS = r'<'
+t_IS_EQ = r'=='
+t_NOT_EQ = r'!='
+t_GREATER_EQ = r'>='
+t_LESS_EQ = r'<='
+t_PLUS_PLUS = r'\+\+'
+t_MINUS_MINUS = r'--'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIV = r'/(?!\*)'
+t_MODULO = r'%'
+t_DOUBLE_AMPERSAND = r'&&'
+t_DOUBLE_PIPE = r'\|\|'
+t_EXCLAMATION = r'!'
+t_AMPERSAND = r'&'
+t_PIPE = r'\|'
+t_CARET = r'\^'
+t_QUESTION = r'\?'
+t_TILDE = r'~'
+t_POUND = r'\#'
+t_EQ_PLUS = r'\+='
+t_EQ_MINUS = r'-='
+t_EQ_TIMES = r'\*='
+t_EQ_DIV = r'/='
+t_EQ_MODULO = r'%='
+t_SINGLE_QUOTE = r'\''
+t_DOUBLE_QUOTE= r'\"'
+t_BACK_SLASH = r'\\'
+
+
+Id=r'[A-Za-z_][\w]*'
+@TOKEN(Id)
+def t_ID(t):
+    """Match an identifier and check if it is a keyword.
+    This approach greatly reduces the number of regular 
+    expression rules and is likely to make things a little faster.
     """
-    #def __init__(self,error_func,type_lookup_func):
-    def __init__(self):
-        """ Create a new Lexer.
+    r'[A-Za-z_][\w]*'
+    global keywords
+    if keywords.has_key(t.value):
+        t.type=keywords[t.value]
+    return t
 
-           error_func:
-                An error function. Will be called with an error
-                message, line and column as arguments, in case of 
-                an error during lexing.
+def t_ILLEGAL_ID(t):
+    r'(?<=[\d])[A-Za-z_][\w]*'
+    print "Ill_formed Identifier %s' at line number %d" % (t.value, t.lineno)
 
-           type_lookup_func:
-                A type lookup function. Given a string, it must
-                return True IFF this string is a name of a type
-                that was defined with a typedef earlier.
-        """
-        #self.error_func = error_func
-        #self.type_lookup_func = type_lookup_func
-        self.filename = ''
+    # Match a decimal number
+def t_DNUMBER(t):
+    r'((\d*)\.((\d*([eE][+-]\d+))|\d+)(?=[+\-*/%(),;\s])|([eE][+-]\d+)(?=[+\-*/()%,;\s]))'
+    return t
 
-    def build(self, **kwargs):
-        """ Builds the lexer from the specification. Must be
-            called after the lexer object is created. 
+# Match an integer
+def t_INUMBER(t):
+    r'\d+(?=[+\-*/()%,;\s])'
+    return t
 
-            This method exists separately, because the PLY
-            manual warns against calling lex.lex inside
-            __init__
-        """
-        self.lexer = lex.lex(object=self, **kwargs)
+def t_DOT(t):
+    r'\.'
+    return t
 
-    def token(self):
-        g = self.lexer.token()
-        return g
-    
+c1=r'[eE]([eE]*[+-]*\d*)*(?=[+\-*/()%,;\s])'
+c2=r'([eE]*[+-]*\d*)*\.(\d*\.*[eE]*[+-]*)*(?=[+\-*/%(),;\s])'
+c3=r'(\d*)\.((\d*([eE][+-]\d+))|\d+)([^+()\-*/%,;\s][a-zA-Z_]*)'
+c4=r'([eE][+-]\d+)[^+()\-*/%,;\s][a-zA-Z_]*'
+c5=r'[eE]([eE]*[+-]*\d*)*([^+\-*/()%,;\s][a-zA-Z_]*)'
+c6=r'([eE]*[+-]*\d*)*\.(\d*\.*[eE]*[+-]*)*([^+\-*/%(),;\s][a-zA-Z_]*)'
+Il_Dnum=r'('+c4+r'|'+c5+r'|'+c6+'|'+c1+r'|'+c2+r'|'+c3+r')'
 
+@TOKEN(Il_Dnum)
 
-    ##
-    ## All the tokens recognized by the lexer
-    ##
-
-    special_characters=('COMMA',
-    'COLON',
-    'SEMICOLON',
-    'LPAREN',
-    'RPAREN',
-    'LBRACKET',
-    'RBRACKET',
-    'LBRACE',
-    'RBRACE',
-    'QUESTION',
-    'TILDE',
-    'POUND',
-    'DOT',    
-    'SINGLE_QUOTE',
-    'DOUBLE_QUOTE',
-    'BACK_SLASH'
-    )
-
-    
-    operators=('ASSIGN','ARROW',
-    'GREATER',
-    'LESS',
-    'IS_EQ',
-    'NOT_EQ',
-    'GREATER_EQ',
-    'LESS_EQ',
-    'PLUS_PLUS',
-    'MINUS_MINUS', 
-    'PLUS',    
-    'MINUS',
-    'TIMES',   
-    'DIV', 
-    'MODULO', 'DOUBLE_AMPERSAND',
-    'DOUBLE_PIPE',
-    'EXCLAMATION',  'AMPERSAND',
-    'PIPE',
-    'CARET',
-    'EQ_PLUS',
-    'EQ_MINUS',
-    'EQ_TIMES',
-    'EQ_DIV',
-    'EQ_MODULO',
-    )
-
-    complex_tokens=('ID',
-    'ILLEGAL_ID',
-    'DNUMBER',
-    'INUMBER',
-    'ILLEGAL_DNUMBER',
-    'ILLEGAL_INUMBER',
-    'LIT_STR',
-    'LIT_CHAR',
-    'COMMENT')
-    
-    ##
-    ## Reserved keywords
-    ##
-    
-    keywords={'bool': 'BOOL',
-            'break': 'BREAK',
-            'case' : 'CASE', 
-            'char'  : 'CHAR',
-            'class' : 'CLASS',
-            'continue' : 'CONTINUE',
-            'default' : 'DEFAULT',
-            'delete' : 'DELETE',
-            'do' : 'DO',
-            'double' : 'DOUBLE',
-            'else' : 'ELSE',
-            'false' : 'FALSE',
-            'float' : 'FLOAT',
-            'for' : 'FOR',
-            'if' : 'IF',
-            'inline' : 'INLINE',
-            'int' : 'INT',
-            'new' : 'NEW',
-            'operator' : 'OPERATOR',
-            'private' : 'PRIVATE',
-            'public' : 'PUBLIC',
-            'protected' : 'PROTECTED',
-            'return' : 'RETURN',
-            'sizeof' : 'SIZEOF',
-            'struct' : 'STRUCT',
-            'switch' :'SWITCH',     
-            'true' :'TRUE',
-            'typename' : 'TYPENAME',
-            'void' : 'VOID',
-            'while' :'WHILE'
-            }
-
-    tokens=special_characters+operators+complex_tokens+tuple(keywords.values())
+def t_ILLEGAL_DNUMBER(t):
+    print "Ill_formed Double Number '%s' at line number %d" % (t.value, t.lineno)
 
 
-    # Each token is specified by writing a regular expression rule. 
-    # Each of these rules are are defined by making declarations with a 
-    # special prefix t_ to indicate that it defines a token.
-    
-    t_ARROW = r'->'
-    t_ASSIGN = r'='
-    t_COMMA = r','
-    t_COLON = r':'
-    t_SEMICOLON = r';'
-    t_LPAREN = r'\('
-    t_RPAREN = r'\)'
-    t_LBRACKET = r'\['
-    t_RBRACKET = r'\]'
-    t_LBRACE = r'{'
-    t_RBRACE = r'}'
-    t_GREATER = r'>'
-    t_LESS = r'<'
-    t_IS_EQ = r'=='
-    t_NOT_EQ = r'!='
-    t_GREATER_EQ = r'>='
-    t_LESS_EQ = r'<='
-    t_PLUS_PLUS = r'\+\+'
-    t_MINUS_MINUS = r'--'
-    t_PLUS = r'\+'
-    t_MINUS = r'-'
-    t_TIMES = r'\*'
-    t_DIV = r'/(?!\*)'
-    t_MODULO = r'%'
-    t_DOUBLE_AMPERSAND = r'&&'
-    t_DOUBLE_PIPE = r'\|\|'
-    t_EXCLAMATION = r'!'
-    t_AMPERSAND = r'&'
-    t_PIPE = r'\|'
-    t_CARET = r'\^'
-    t_QUESTION = r'\?'
-    t_TILDE = r'~'
-    t_POUND = r'\#'
-    t_EQ_PLUS = r'\+='
-    t_EQ_MINUS = r'-='
-    t_EQ_TIMES = r'\*='
-    t_EQ_DIV = r'/='
-    t_EQ_MODULO = r'%='
-    t_SINGLE_QUOTE = r'\''
-    t_DOUBLE_QUOTE= r'\"'
-    t_BACK_SLASH = r'\\'
+def t_ILLEGAL_INUMBER(t):
+    r'\d+([^+\-*/()%,;\s][a-zA-Z_]*)'
+    print "Ill_formed Integer Number '%s' at line number %d" % (t.value, t.lineno)
 
-    
-    Id=r'[A-Za-z_][\w]*'
-    @TOKEN(Id)
-    def t_ID(self,t):
-        """Match an identifier and check if it is a keyword.
-        This approach greatly reduces the number of regular 
-        expression rules and is likely to make things a little faster.
-        """
-        r'[A-Za-z_][\w]*'
-        if self.keywords.has_key(t.value):
-            t.type=self.keywords[t.value]
-        return t
+def t_LIT_CHAR(t):
+    r'\'[\w\W]\''
+    return t
 
-    def t_ILLEGAL_ID(self,t):
-        r'(?<=[\d])[A-Za-z_][\w]*'
-        print "Ill_formed Identifier %s' at line number %d" % (t.value, t.lineno)
+def t_LIT_STR(t):
+    r'"[^\n]*?(?<!\\)"'
+    temp_str = t.value.replace(r'\\', '')
+    m = re.search(r'\\[^n"]', temp_str)
+    if m != None:
+        print "Line %d. Unsupported character escape %s in string literal." % (t.lineno, m.group(0))
+        return
+    return t
 
-        # Match a decimal number
-    def t_DNUMBER(self,t):
-        r'((\d*)\.((\d*([eE][+-]\d+))|\d+)(?=[+\-*/%(),;\s])|([eE][+-]\d+)(?=[+\-*/()%,;\s]))'
-        return t
+# Match single line and multiline comments and 
+    # increase the line number
+def t_COMMENT(t):
+    r'(/\*[\w\W]*?\*/)|(//[\w\W]*?\n)'
+    t.lineno += t.value.count('\n')
+    pass
 
-        # Match an integer
-    def t_INUMBER(self,t):
-        r'\d+(?=[+\-*/()%,;\s])'
-        return t
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
 
-    def t_DOT(self,t):
-        r'\.'
-        return t
+# The use of t_ignore provides substantially better lexing performance 
+# because it is handled as a special case and is checked in a much more 
+# efficient manner than the normal regular expression rules.
+t_ignore = '[ \t\r\f\v]'
 
-    c1=r'[eE]([eE]*[+-]*\d*)*(?=[+\-*/()%,;\s])'
-    c2=r'([eE]*[+-]*\d*)*\.(\d*\.*[eE]*[+-]*)*(?=[+\-*/%(),;\s])'
-    c3=r'(\d*)\.((\d*([eE][+-]\d+))|\d+)([^+()\-*/%,;\s][a-zA-Z_]*)'
-    c4=r'([eE][+-]\d+)[^+()\-*/%,;\s][a-zA-Z_]*'
-    c5=r'[eE]([eE]*[+-]*\d*)*([^+\-*/()%,;\s][a-zA-Z_]*)'
-    c6=r'([eE]*[+-]*\d*)*\.(\d*\.*[eE]*[+-]*)*([^+\-*/%(),;\s][a-zA-Z_]*)'
-    Il_Dnum=r'('+c4+r'|'+c5+r'|'+c6+'|'+c1+r'|'+c2+r'|'+c3+r')'
+    # Called when no rule is matched
+    # t.value attribute contains the rest of the input string 
+    # that has not been tokenized
+    # we simply print the offending character and skip ahead 
+    # one character by calling t.lexer.skip(1)
 
-    @TOKEN(Il_Dnum)
-    
-    def t_ILLEGAL_DNUMBER(self,t):
-         print "Ill_formed Double Number '%s' at line number %d" % (t.value, t.lineno)
-        
+def t_error(t):
+    print "Illegal character '%s' at line number %d" % (t.value[0], t.lineno)
+    t.lexer.skip(1)
 
-    def t_ILLEGAL_INUMBER(self,t):
-        r'\d+([^+\-*/()%,;\s][a-zA-Z_]*)'
-        print "Ill_formed Integer Number '%s' at line number %d" % (t.value, t.lineno)
-        
-    def t_LIT_CHAR(self,t):
-        r'\'[\w\W]\''
-        return t
-    
-    def t_LIT_STR(self,t):
-        r'"[^\n]*?(?<!\\)"'
-        temp_str = t.value.replace(r'\\', '')
-        m = re.search(r'\\[^n"]', temp_str)
-        if m != None:
-            print "Line %d. Unsupported character escape %s in string literal." % (t.lineno, m.group(0))
-            return
-        return t
-    
-        # Match single line and multiline comments and 
-        # increase the line number
-    def t_COMMENT(self,t):
-        r'(/\*[\w\W]*?\*/)|(//[\w\W]*?\n)'
-        t.lineno += t.value.count('\n')
-        pass
+def test_lexer(lexer, string):
+    lexer.input(string)
+    # Tokenize
+    while True:
+        tok = lexer.token()
+        if not tok: 
+            break      # No more input
+        print tok
 
-    def t_newline(self,t):
-        r'\n+'
-        t.lexer.lineno += len(t.value)
-        
-    # The use of t_ignore provides substantially better lexing performance 
-    # because it is handled as a special case and is checked in a much more 
-    # efficient manner than the normal regular expression rules.
-    t_ignore = '[ \t\r\f\v]'
-
-        # Called when no rule is matched
-        # t.value attribute contains the rest of the input string 
-        # that has not been tokenized
-        # we simply print the offending character and skip ahead 
-        # one character by calling t.lexer.skip(1)
-
-    def t_error(self,t):
-        print "Illegal character '%s' at line number %d" % (t.value[0], t.lineno)
-        t.lexer.skip(1)
-
-
-    def test(self,data):
-        self.lexer.input(data)
-        while True:
-            tok= self.token()
-            if not tok:
-                break
-            print tok
-
-        
-def run_lexer():
-
+if __name__ == '__main__':
     file = open(sys.argv[1])
     lines = file.readlines()
     file.close()
     strings = ""
     for i in lines:
         strings += i
-    Lexer= CPPLexer()
-    Lexer.build()
-    Lexer.test(strings)
-
-if __name__ == '__main__':
-    run_lexer()
+    # run lexer on input
+    lexer = lex.lex()
+    test_lexer(lexer, strings)
