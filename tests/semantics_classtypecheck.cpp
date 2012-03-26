1c958d181885524a450f75c53066ed20e779534e
Atul Agarwal <atulagarwal104@gmail.com>
2012-03-26 21:15:56 +0530

    Test cases added

---
+++ tests/semantics_classtypecheck.cpp
@@ -0,0 +1,24 @@
+//Class type checking 
+class Animal
+{
+    int a,b;
+};
+class Animal2
+{
+    int a,b;
+    int func(){
+        int c;
+        c =10;
+    }
+};
+int main()
+{
+    Animal x,y,z;
+    Animal2 p,q,r;
+    //Animal t = r;
+    int e;
+    //Animal u = e;
+    //Animal2 v = x;
+    
+    return 0;
+}
---
+++ tests/semantics_typecheck.cpp
@@ -0,0 +1,12 @@
+//Simple type Checking
+
+int main()
+{
+    int a,b,c;
+    char d = 'a';
+    //float a;
+    float e,f;
+    //int g = f;
+    int h = a,i=3;
+    return 0;
+}
