753e579f5b4f5ebb9bea5104fe1d6997446c820c
Himanshu Agarwal <himanshu.224a@gmail.com>
2012-03-26 21:08:35 +0530

    FUnction_test_cases

--- tests/semantics_function_call.cpp
+++ tests/semantics_function_call.cpp
@@ -4,9 +4,13 @@ int func(int a , char b)
 {
     return a;
 }
-
+int main()
+{
 int x;
 int y = 5;
 char z = 'a';
-x = func(y,z);
+x=func(y,z);
+//func(y,y);
+//func(y);
 // check function calling
+}
