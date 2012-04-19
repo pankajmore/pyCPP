a1d64c8be03e0c248420f08a0224b08e09ea780e
Pankaj More <pankajmore@gmail.com>
2012-04-19 16:01:04 +0530

    script corrected

--- Final_tests/opttest.sh
+++ Final_tests/opttest.sh
@@ -4,5 +4,5 @@ cd ../pyCPP/
 python parser.py ../Final_tests/$1
 cd ../Final_tests/
 echo "Running test"
-./optimzer $bar.asm > ${bar}o.asm
+../pyCPP/optimizer $bar.asm > ${bar}o.asm
 spim file ${bar}o.asm
